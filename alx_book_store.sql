5e896b026e40345764c4ef0bc2d4415024bc891d
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-06 21:54:50 +0300

    Add: Initialize the Database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,7 +6,7 @@ CREATE TABLE Authors(
 );
 
 CREATE TABLE Books(
-    book_id (Primary Key),
+    book_id (PRIMARY KEY),
     title VARCHAR(130),
     author_id (Foreign Key referencing Authors table),
     price DOUBLE,
@@ -15,20 +15,20 @@ CREATE TABLE Books(
 
 
 CREATE TABLE Customers(
-    customer_id (Primary Key),
+    customer_id (PRIMARY KEY),
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Orders(
-    order_id (Primary Key),
+    order_id (PRIMARY KEY),
     customer_id (Foreign Key referencing Customers table),
     order_date DATE
 );
 
 CREATE TABLE Order_Details(
-    orderdetailid (Primary Key),
+    orderdetailid (PRIMARY KEY),
     order_id (Foreign Key referencing Orders table),
     book_id (Foreign Key referencing Books table),
     quantity DOUBLE
